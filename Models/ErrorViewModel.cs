14d4a1e3ae56209cfcb76b604de8dc76889e943b
eduardstefanica <eduardstefanicacard@gmail.com>
2024-05-09 17:27:32 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppComboCascadeEFCL_EduardSt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
